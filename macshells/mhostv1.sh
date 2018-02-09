14d1939d718c38fc86d666d578324a2e32028db2
timc <metacman@gmail.com>
2018-02-09 14:09:48 -0600

    added the new file mhostv1.sh as a copy of hostv1.sh renamed the file to standard mac naming (first letter 'm)

---
+++ macshells/mhostv1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# exercise 3
+clear
+HOSTNAME=$(hostname)
+echo "..."
+echo "****   "This script is running on Mac computer name ${HOSTNAME} ."  ****"
+sleep 05
+echo "..."
+echo "..."
+read -n 1 -s -r -p "Press any key to clear the screen and continue"
+clear
