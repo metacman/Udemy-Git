b0727cd213b1692573e193280cde01132ce085cb
timc <metacman@gmail.com>
2018-02-09 14:08:49 -0600

    added some pauses and UI echos to the mdog.sh mac shell script

--- macshells/mdog.sh
+++ macshells/mdog.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 #exercise 5
 # displaysman, ber, pig, dog, cat nd sheep to the screen
+echo "Running an ls -laG first"
 ls -laG
+echo "pausing 5 seconds .."
 sleep 05
 clear
-echo 
+echo "now displaying the variable with words filled in one per line .."
+sleep 05
 echo
 for NAME in man bear pig dog kitty sheep
 do
