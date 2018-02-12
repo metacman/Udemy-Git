aba9c5c2b333089ecd12d3e95b3c46a4ece289fa
metacman <34141448+metacman@users.noreply.github.com>
2018-02-12 10:09:24 -0600

    minor mods in hostv1.sh  took out reference to Windows in line 6 and resuded pause time from 5 to 3 seconds in line 7

--- LinuxShells/hostv1.sh
+++ LinuxShells/hostv1.sh
@@ -3,10 +3,9 @@
 clear
 HOSTNAME=$(hostname)
 echo "..."
-echo "****   "This script is running on Windows computer name ${HOSTNAME}."  ****"
-sleep 05
+echo "****   "This script is running on computer name ${HOSTNAME}."  ****"
+sleep 03
 echo "..."
 echo "..."
 read -n 1 -s -r -p "Press any key to clear the screen and continue"
 clear
-
