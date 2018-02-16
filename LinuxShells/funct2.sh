e56f97d2f04f1e84b132be898d1cd1f207394f4b
claytonta <me_tacman@hotmail.com>
2018-02-16 07:55:02 -0600

    added two new files

---
+++ LinuxShells/funct2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+function file_count() {
+   local DIR=$1
+   local NUMBER_OF_FILES=$(ls -al $DIR | wc -l)
+   echo "${DIR}:"
+   echo "$NUMBER_OF_FILES"
+}
+
+file_count ~
+file_count ~/Desktop
+file_count ~/Documents
