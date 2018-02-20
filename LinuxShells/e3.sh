72852d2578081799235e1e4439bb70a956db392c
claytonta <me_tacman@hotmail.com>
2018-02-20 13:13:05 -0600

    added 3-4 echo lines to the script e3.sh to better explain its functionality

--- LinuxShells/e3.sh
+++ LinuxShells/e3.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-
+echo "following is the content of the /etc/networks file"
+echo "..."
 cat /etc/networks
 if [ "$?" -eq "0" ]
 then
-  echo "Command succeeded, exit status=$?"
+  echo "cat Command succeeded, exit status=$?"
   exit 0
+echo "..."
 else
-  echo "Command failed, exit status=$?"
+  echo "cat Command failed, exit status=$?"
   exit 1
+echo "..."
 fi
 
