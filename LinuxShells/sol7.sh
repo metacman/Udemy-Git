69f9459ee20edf1e5d8c7f344143b101ebd8dbf1
claytonta <me_tacman@hotmail.com>
2018-04-11 12:25:57 -0500

    Made several minor mods to sol7.sh

--- LinuxShells/sol7.sh
+++ LinuxShells/sol7.sh
@@ -4,12 +4,25 @@ FILE=$1
 
 if [ -f "$FILE" ]
 then
+echo "..."
+echo "..."
   echo "$FILE is a regular file."
+echo "..."
+echo "..."
 elif [ -d "$FILE" ]
 then
-  echo "$FILE is a directory."
+echo "$FILE is a directory."
+echo "..."
+echo "..."
 else
   echo "$FILE is something other than a regular file or directory."
+echo "..."
+echo "..."
 fi
 
 ls -l $FILE
+echo "..."
+echo "..."
+read -n 1 -s -r -p "Press any key to clear the screen and continue"
+clear
+
