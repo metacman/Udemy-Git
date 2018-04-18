57b15d39154f76a32ce4c75c32d036d45cd5ae6d
metacman <34141448+metacman@users.noreply.github.com>
2018-04-18 07:29:31 -0500

    Updated lor-cli.sh with a minor correction in the comment in line 3

--- LinuxShells/lor-cli.sh
+++ LinuxShells/lor-cli.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This shell script will provide a recursive long list ('ll') based on the users command line input
-# For example: a long list recursive of the /c/ (root)directory using input from command line whould be 'sh lor.sh /c/'
+# For example: a long list recursive of the /c/ (root)directory using input from command line whould be 'sh lor-cli.sh /c/'
 if [ -d "$1" ] 
 then 
 ls -alR "$1" | less
