d43d75b1fab4945e5854350114ee348e01587a39
claytonta <me_tacman@hotmail.com>
2018-05-18 09:51:28 -0500

    revamped the funct1.sh script to echo what the script is doing (a file count) and pwd so the user can see what directory the file count is from

--- LinuxShells/funct1.sh
+++ LinuxShells/funct1.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-
+clear
+ echo This script counts the number of files in the directory where it is run
+ echo
+ read -n 1 -s -r -p "Press any key to clear the screen and continue"
+ clear
 function file_count() {
    local NUMBER_OF_FILES=$(ls | wc -l)
-   echo "$NUMBER_OF_FILES"
+   echo
+   echo "$(pwd) has a file count of: "$NUMBER_OF_FILES""
 }
 
 file_count
+echo
+echo
+ls -al
